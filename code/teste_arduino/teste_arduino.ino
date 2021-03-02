59616bf90781f13f6b358c90b3c44282c7843ed2
Carolina-Pinheiro <57190987+Carolina-Pinheiro@users.noreply.github.com>
2021-03-02 20:14:46 +0100

    Upgrades
    Reads from the arduino every data, see the first line in "datafile" to see the format of the data output

--- code/teste_arduino/teste_arduino.ino
+++ code/teste_arduino/teste_arduino.ino
@@ -1,11 +1,14 @@
-float lat=30;
-float lon=35;
+float lat=41.804;
+float lon=-6.749;
 float alt=5;
 float aceX=5;
 float aceY=6;
 float aceZ=7;
 float altMed=6;
-  
+float vel=10;
+float pitch=10;
+float roll=15;
+float yaw=20;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -21,7 +24,12 @@ void loop() {
   aceY= aceY + 0.1 ;
   aceZ= aceZ + 0.1 ;
   altMed= altMed + 4.9 ;
-  String gps= String(aceX) + ' '  + String(aceY) + ' ' + String(aceZ) + ' ' + String(lat, 5) + ' ' + String(lon,5) + ' ' + String(alt,5) + ' ' + String(altMed,5);
+  vel=vel+0.5;
+  pitch=pitch+1;
+  roll=roll+1;
+  yaw=yaw+1;
+  
+  String gps= String(aceX) + ' '  + String(aceY) + ' ' + String(aceZ) + ' ' + String(lat, 5) + ' ' + String(lon,5) + ' ' + String(alt,5) + ' ' + String(altMed,5) + ' ' + String(vel,3) + ' ' + String(pitch,3) + ' ' + String(roll,3) + ' ' + String(yaw,3);
   String data= gps ;
   Serial.println(data);
   delay(1000);
